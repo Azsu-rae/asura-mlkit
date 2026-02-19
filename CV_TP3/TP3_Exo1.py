edc0e0c46a213824382e5182e0260745cbbd319a
Azsu-rae <aitameurmedilyas@gmail.com>
2026-02-19 21:50:39 +0100

    sync

--- CV_TP3/TP3_Exo1.py
+++ CV_TP3/TP3_Exo1.py
@@ -1,12 +1,12 @@
 
-import cvkit
 import cv2
 
 im1 = cv2.imread("Images/compteur.jpg", cv2.IMREAD_GRAYSCALE)
 if im1 is None:
     raise ValueError("No image!")
 
-otsu_threshold, im2 = cv2.threshold(im1, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+otsu_threshold, im2 = cv2.threshold(
+    im1, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
 cv2.imshow('original', im1)
 cv2.imshow('binarized', im2)
