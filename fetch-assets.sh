c7addf58d5f28643b802ec1e9400b837f27343f4
Azsu-rae <aitameurmedilyas@gmail.com>
2026-08-04 02:00:06 +0300

    flaky download option

--- fetch-assets.sh
+++ fetch-assets.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 set -e
 
-I2A_S1=https://drive.google.com/drive/folders/1uNQucxKvYcuv8UwWKcDSsUk9K_IzskxY?usp=drive_link
+TAI_IMAGES=https://drive.google.com/drive/folders/1Q3pt4DxMoC6d35xCjBFh5pw89BAINSUD?usp=sharing
 
-if [ ! -d "$HOME/Documents/I2A_S1" ]; then
-    echo "$HOME/Documents/I2A_S1 found. Download the drive folder at:"
-    echo "      $I2A_S1"
+ans=""
+if [ ! -d "./TAI" ]; then
+    echo "you can find the dataset in"
     echo ""
+    echo "          $TAI_IMAGES"
+    echo ""
+    echo "and then place them in this directory. or,"
+    echo "an attempt at downloading them directly can be made using gdown (could fail due to google throttling)."
+    read -p "attempt [y/n]? " ans
+    if [[ ! "$ans" =~ ^[Yy]$ ]]; then
+        ans=""
+    fi
+
+    if [[ -n "$ans" ]]; then
+        if ! command -v gdown > /dev/null; then
+            echo "gdown is required to fetch from google drive"
+            echo "install using \`pipx install gdown\`"
+        else
+            gdown --folder $TAI_IMAGES
+        fi
+    else
+        echo "skipped."
+    fi
 else
-    ln -sfn $HOME/Documents/I2A_S1/TAI/Images/ ./
-    ln -sfn $HOME/Documents/I2A_S1/TAI/Images_TP2_EXO6_SR/ ./
+    echo "./TAI already exists in this directory. aborting."
 fi
